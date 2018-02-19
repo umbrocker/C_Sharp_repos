42b1815a9555b323736f6365da1424ca1b251039
Umi <umbrocker@gmail.com>
2018-02-19 12:02:08 +0100

    Wpf_basics_01

--- Wpf_basics/Wpf_basics/MainWindow.xaml.cs
+++ Wpf_basics/Wpf_basics/MainWindow.xaml.cs
@@ -24,5 +24,40 @@ namespace Wpf_basics
         {
             InitializeComponent();
         }
+
+        private void ApplyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The description is: {this.DescriptionTxtBox.Text}");
+        }
+
+        private void ResetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WeldChbx.IsChecked = this.AssemblyChbx.IsChecked = this.PlasmaChbx.IsChecked = this.LaserChbx.IsChecked = this.LatheChbx.IsChecked = this.FoldChbx.IsChecked = 
+                this.PurchaseChbx.IsChecked = this.RollChbx.IsChecked = this.DrillChbx.IsChecked = this.SawChbx.IsChecked = false;
+        }
+
+        private void Checkbox_checked(object sender, RoutedEventArgs e)
+        {
+            this.LengthTextbox.Text += ((CheckBox)sender).Content;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.NoteTxtbx == null)
+                return;
+            var combo = (ComboBox)sender;
+            var value = (ComboBoxItem)combo.SelectedValue;
+            this.NoteTxtbx.Text = (string)value.Content;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComboBox_SelectionChanged(this.FinishDropdown, null);
+        }
+
+        private void SupplierNameTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.MassTxtBx.Text = this.SupplierNameTxt.Text;
+        }
     }
 }
